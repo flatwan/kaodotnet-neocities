fa92753c2d3e3d649cf67188ce179409f3670dce
Ewan Nyai (flatwan) <nadewan1409@gmail.com>
2026-07-21 05:32:02 +0200

    initial

---
+++ neocities/js/include.js
@@ -0,0 +1,17 @@
+fetch("./components/top_bar.html")
+    .then(response => {
+        console.log(response.status);
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById("top_bar-placeholder").innerHTML = data;
+    });
+
+fetch("./components/bot_bar.html")
+    .then(response => {
+        console.log(response.status);
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById("bot_bar-placeholder").innerHTML = data;
+    });
